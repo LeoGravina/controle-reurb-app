f40558b3320d88bb3d58bc12827eb9540b21e1b0
Leonardo Gravina <leonardocarlos807@gmail.com>
2025-10-23 08:16:05 -0300

    adicionei o nome do Dan e estilização

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,7 +5,7 @@ function Footer() {
     return (
         <footer className="main-footer">
             <div className="container footer-content">
-                <p>Este projeto foi desenvolvido por <strong>Leonardo Gravina Carlos</strong>.</p>
+                <p>Este projeto foi desenvolvido por <strong>Leonardo Gravina Carlos(programador) e Dan Kenner</strong>.</p>
                 <p>
                     Quer desenvolver um projeto? <Link to="/contato">Entre em contato</Link>.
                 </p>
--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -2,13 +2,11 @@
 
 import React from 'react';
 import { Outlet } from 'react-router-dom';
-import Footer from '../components/Footer'; // Importando o Footer
+import Footer from '../components/Footer';
 
-// Este componente agora é responsável apenas pela estrutura e pelo rodapé.
 function MainLayout() {
     return (
-        <div className="site-wrapper">
-            {/* O Outlet renderizará o componente da rota (App, ProfilePage, etc.) */}
+        <div className="site-wrapper"> 
             <Outlet />
             <Footer />
         </div>
